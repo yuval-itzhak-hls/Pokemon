8849b0f7e0333d0deb7bccc5960b478bb5149ab4
yuval-itzhak-hls <yuvali@moveohls.com>
2025-06-26 15:04:14 +0300

    refactor(AppLayout): use const object based approach inside the file instead of hard coded string.

--- src/pages/home-page/AppLayout.tsx
+++ src/pages/home-page/AppLayout.tsx
@@ -2,9 +2,14 @@ import React, { useState } from "react";
 import { Outlet } from "react-router-dom";
 import { AppHeader } from "./AppHeader";
 
+export const pokemonTabs = 
+{ all: { key: "all" , value: "all pokemons" },
+ mine: { key: "mine" , value: "my pokemons" } }
+
 export function AppLayout() {
-  const [activeTab, setActiveTab] = useState("all pokemons");
-  const showMyPokemons = activeTab === "my pokemons";
+  
+  const [activeTab, setActiveTab] = useState(pokemonTabs.all.value);
+  const showMyPokemons = activeTab === pokemonTabs.mine.value;
 
   return (
     <div className="min-h-screen flex flex-col bg-bg-system px-16 py-16">
