cef88b68b000344b3c1b88a0c109251f5e25d5d3
yuval-itzhak-hls <yuvali@moveohls.com>
2025-07-01 23:56:38 +0300

    Adding search abilliaty to drop down and fixing style.

--- src/pages/fighting-arena-page/LiveFightScreen.tsx
+++ src/pages/fighting-arena-page/LiveFightScreen.tsx
@@ -13,14 +13,19 @@ import CatchPanel from "./CatchPanel";
 import closePokemon from "@/assets/close-pokemon.png"
 import { Status, FightMessage } from "./messages/FightMessage";
 import { MessageCard } from "./messages/MessageCard";
+import { GenericDropDown } from "@/design-system/generic-componenets/GenericDropDown";
+import type { Options } from "@/design-system/generic-componenets/GenericDropDown";
+import { useBattle } from "@/context/BattleContext";
 
 
 const STORAGE_KEY = "myPokemons";
+
 
 export const LiveFightScreen: React.FC<ChosenPokemonDisplayProps> = ({
   userPokemon,
   opponentPokemon,
 }) => {
+  const { setBattle } = useBattle();
   const navigate = useNavigate();
   const { userLife, opponentLife, applyAttack, rematch } = useLifePoints();
 
@@ -34,6 +39,9 @@ export const LiveFightScreen: React.FC<ChosenPokemonDisplayProps> = ({
   const [isAbleCatch, setIsAbleCatch] = useState<boolean>(false);
   const [caught, setCaught] = useState<boolean>(false);
   const [status, setStatus] = useState<Status>(Status.start);
+  const [selectedId, setSelectedId] = useState<string | undefined>(undefined);
+  const [isSwitch, setIsSwitch] = useState<boolean>(false);
+  const [newUserPokemon, setNewUserPokemon] = useState<Pokemon>(userPokemon);
 
   const maxCatchTries = 3;
   const lowHpThreshold = opponentPokemon.hpLevel * 0.2;
@@ -116,7 +124,7 @@ export const LiveFightScreen: React.FC<ChosenPokemonDisplayProps> = ({
     setCatchTries(0);
     setIsAbleCatch(false);
     setCaught(false);
-    setStatus("SWITCH");
+    setStatus(Status.switch);
   };
 
 
@@ -125,8 +133,21 @@ export const LiveFightScreen: React.FC<ChosenPokemonDisplayProps> = ({
     setCatchTries(0);
     setIsAbleCatch(false);
     setCaught(false);
-    setStatus("SWITCH");
+    setStatus(Status.switch);
   };
+
+  const handleSwitchSelection = (newId: string) => {
+      setSelectedId(newId);
+      const chosen = myPokemons.find((p) => p.id.toString() === newId);
+      if (!chosen) return;
+      setNewUserPokemon(chosen)
+      setCaught(false);
+      setCatchTries(0);
+      setIsAbleCatch(false);
+      setIsSwitch(true);
+      setBattle(chosen ,opponentPokemon);
+      
+    };
 
   const { pokemons: allPokemons } = usePokemonsData({
     showMyPokemons: false,
@@ -141,6 +162,14 @@ export const LiveFightScreen: React.FC<ChosenPokemonDisplayProps> = ({
     rowsPerPage: 999,
   });
 
+   const pokemonOptions: Options[] = myPokemons.map((p) => ({
+    value: p.id.toString(),
+    label: p.name,
+    subLabel: p.speed,
+    img: p.image,
+    disabled: false,
+  }));
+
   if (showChoose) {
     return (
       <ChoosePokemonBattlePanel
@@ -154,106 +183,118 @@ export const LiveFightScreen: React.FC<ChosenPokemonDisplayProps> = ({
   if (!userPokemon || !opponentPokemon) return null;
 
   return (
-    <div
-      className="relative mx-auto w-[90%] h-[400px] md:h-[400px] lg:h-[650px] bg-cover bg-center"
-      style={{ backgroundImage: `url(${fightArena})` }}
-    >
-      {/* Opponent life bar */}
-      <LifeBarCard
-        name={opponentPokemon.name}
-        speed={opponentPokemon.speed}
-        life={opponentLife}
-        max={opponentPokemon.hpLevel}
-        isActive={!isUserTurn}
-        className="absolute top-4 right-4"
-      />
-
-      {/* User life bar */}
-      <LifeBarCard
-        name={userPokemon.name}
-        speed={userPokemon.speed}
-        life={userLife}
-        max={userPokemon.hpLevel}
-        isActive={isUserTurn}
-        className="absolute bottom-4 left-4"
-      />
-
-      {/* pokemons and attack button */}
-      <img
-        src={userPokemon.image}
-        alt="Your Pokémon"
-        className="absolute left-[20%] top-[60%] w-1/3 md:w-1/4 lg:w-[20%] h-[30%] object-contain transform -translate-y-1/4"
-      />
-      <img
-        src={!caught ? opponentPokemon.image : closePokemon}
-        alt="Opponent Pokémon"
-        className="absolute right-[25%] top-[30%] w-1/3 md:w-1/4 lg:w-[20%] h-[30%] object-contain transform -translate-y-3/4"
-      />
-
-      {/* Attack button */}
-      <div
-        className={`absolute bottom-4 right-[14%] ${
-          !isUserTurn ? "pointer-events-none opacity-50" : ""
-        }`}
-      >
-        <AttackButton onClick={handleAttack} hover={isUserTurn} visible={isUserTurn} />
-      </div>
-
-      {/* Catch button (only on user turn) */}
-      {isUserTurn && (
-        <div className="absolute bottom-4 right-[3%]">
-          <CatchButton
-            onClick={catchPokemon}
-            isAbleCatch={isAbleCatch}
-          />
-        </div>
-      )}
-
-      {/* Win/Loss panel */}
-      <LostWonPanel
-        open={showResult}
-        onOpenChange={setShowResult}
-        name={isLost ? userPokemon.name : opponentPokemon.name}
-        sprite={isLost ? userPokemon.image : opponentPokemon.image}
-        result={isLost ? "lost" : "won"}
-        onSwitch={onSwitchPokemon}
-        onRematch={onRematch}
-        onEnd={onEndMatch}
-      />
-
-      { caught && (
-          <CatchPanel
-        isOpen={caught}
-        onClose={() => setCaught(false)}
-        onContinue={onContinueBattle}
-        onEnd={onEndMatch}
-        name={opponentPokemon.name}
-        imageSrc={opponentPokemon.image}
-        rewards={{
-          speed: opponentPokemon.speed,
-          category: Array.isArray(opponentPokemon.type)
-          ? opponentPokemon.type.join(", ")
-          : opponentPokemon.type || "Unknown",
-          abilities: opponentPokemon.abilities.join(", "),
-        }}
-      />
-      )}
-
-      <div className="absolute top-[9%] left-1/5 transform ">
-      <MessageCard>
-        <FightMessage
-          status={status}
-          attackerName={
-            isUserTurn ? userPokemon.name : opponentPokemon.name
-          }
-          defenderName={
-            isUserTurn ? opponentPokemon.name : userPokemon.name
-          }
+    <div>
+      <div className="m-2 pl-3">
+        <GenericDropDown
+          placeholder={newUserPokemon.name}
+          options={pokemonOptions}
+          value={selectedId}
+          onValueChange={handleSwitchSelection}
+          className="w-[258px] "
+          disabled={isSwitch ? true : false}
+          isSearch={true}
         />
-      </MessageCard>
-      
-    </div>
-      
-    </div>
+      </div>
+      <div
+        className="relative mx-auto w-[97%] h-[400px] md:h-[400px] lg:h-[700px] bg-auto bg-repeat-round "
+        style={{ backgroundImage: `url(${fightArena})` }}
+      >
+        {/* Opponent life bar */}
+        <LifeBarCard
+          name={opponentPokemon.name}
+          speed={opponentPokemon.speed}
+          life={opponentLife}
+          max={opponentPokemon.hpLevel}
+          isActive={!isUserTurn}
+          className="absolute top-4 right-4"
+        />
+
+        {/* User life bar */}
+        <LifeBarCard
+          name={userPokemon.name}
+          speed={userPokemon.speed}
+          life={userLife}
+          max={userPokemon.hpLevel}
+          isActive={isUserTurn}
+          className="absolute bottom-4 left-4"
+        />
+
+        {/* pokemons and attack button */}
+        <img
+          src={userPokemon.image}
+          alt="Your Pokémon"
+          className="absolute left-[20%] top-[60%] w-1/3 md:w-1/4 lg:w-[20%] h-[30%] object-contain transform -translate-y-1/4"
+        />
+        <img
+          src={!caught ? opponentPokemon.image : closePokemon}
+          alt="Opponent Pokémon"
+          className="absolute right-[25%] top-[30%] w-1/3 md:w-1/4 lg:w-[20%] h-[30%] object-contain transform -translate-y-3/4"
+        />
+
+        {/* Attack button */}
+        <div
+          className={`absolute bottom-4 right-[14%] ${
+            !isUserTurn ? "pointer-events-none opacity-50" : ""
+          }`}
+        >
+          <AttackButton onClick={handleAttack} hover={isUserTurn} visible={isUserTurn} />
+        </div>
+
+        {/* Catch button (only on user turn) */}
+        {isUserTurn && (
+          <div className="absolute bottom-4 right-[3%]">
+            <CatchButton
+              onClick={catchPokemon}
+              isAbleCatch={isAbleCatch}
+            />
+          </div>
+        )}
+
+        {/* Win/Loss panel */}
+        <LostWonPanel
+          open={showResult}
+          onOpenChange={setShowResult}
+          name={isLost ? userPokemon.name : opponentPokemon.name}
+          sprite={isLost ? userPokemon.image : opponentPokemon.image}
+          result={isLost ? "lost" : "won"}
+          onSwitch={onSwitchPokemon}
+          onRematch={onRematch}
+          onEnd={onEndMatch}
+        />
+
+        { caught && (
+            <CatchPanel
+          isOpen={caught}
+          onClose={() => setCaught(false)}
+          onContinue={onContinueBattle}
+          onEnd={onEndMatch}
+          name={opponentPokemon.name}
+          imageSrc={opponentPokemon.image}
+          rewards={{
+            speed: opponentPokemon.speed,
+            category: Array.isArray(opponentPokemon.type)
+            ? opponentPokemon.type.join(", ")
+            : opponentPokemon.type || "Unknown",
+            abilities: opponentPokemon.abilities.join(", "),
+          }}
+        />
+        )}
+
+        <div className="absolute top-[9%] left-1/5 transform ">
+        <MessageCard>
+          <FightMessage
+            status={status}
+            attackerName={
+              isUserTurn ? userPokemon.name : opponentPokemon.name
+            }
+            defenderName={
+              isUserTurn ? opponentPokemon.name : userPokemon.name
+            }
+          />
+        </MessageCard>
+        
+      </div>
+    </div>    
+  </div>
   );
 };
