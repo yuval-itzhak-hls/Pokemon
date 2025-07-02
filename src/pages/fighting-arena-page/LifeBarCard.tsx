c9aae26a8253d64af6a5712ff7bba87ade81619f
yuval-itzhak-hls <yuvali@moveohls.com>
2025-07-02 13:49:17 +0300

    fixing colors and sizing.

--- src/pages/fighting-arena-page/LifeBarCard.tsx
+++ src/pages/fighting-arena-page/LifeBarCard.tsx
@@ -2,6 +2,7 @@
 import React from "react";
 import { Card, CardHeader, CardTitle, CardContent, CardFooter } from "@/components/ui/card";
 import { Progress } from "@/components/ui/progress";
+import clsx from "clsx";
 
 interface LifeBarCardProps {
   name: string;
@@ -12,46 +13,49 @@ interface LifeBarCardProps {
   className?: string; 
 }
 
-export const LifeBarCard: React.FC<LifeBarCardProps> = ({
+export const LifeBarCard = ({
   name,
   speed,
   life,
   max,
   isActive,
   className = "",
-}) => {
+}: LifeBarCardProps) => {
+
   const percent = Math.max(0, Math.min(100, Math.round((life / max) * 100)));
 
   // Determine indicator color classes
   const getIndicatorClass = () => {
-    if (life === 0) return "bg-gray-600";
-    if (percent < 30) return "bg-red-600";
-    if (percent < 100) return "bg-yellow-400";
-    return "bg-green-400";
+    if (life === 0) return  "bg-gray-600";
+    if (percent < 30) return isActive? "bg-error-red" : "bg-error-red/80";
+    if (percent < 100) return isActive ? "bg-warning-yellow" : "bg-warning-yellow/80";
+    return isActive ?  "bg-success-green" : "bg-success-green/80";
   };
-  
-  const faintGradient =
-    "linear-gradient(180deg, rgba(247, 91, 91, 0.5) 0%, rgba(221, 113, 80, 0.5) 99.99%, rgba(215, 186, 69, 0.2) 100%)";
+    const faintGradient =
+    "linear-gradient(180deg, rgba(247, 91, 91, 0.6) 0%, rgba(221, 113, 80, 0.5) 99.99%, rgba(215, 186, 69, 0.2) 100%)";
 
   const cardBgClass = life === 0
   ? { background: faintGradient }
   : {};
-
+  
   return (
     <Card
-      className={`w-64 h-[103px] ${life !== 0 ? "bg-gradient-to-br from-teal-300 to-green-300" : ""} border-white border rounded-sm shadow-lg p-0 ${
-        isActive ? "opacity-100" : "opacity-50"
-      } ${className}`}
-      style={cardBgClass}
-    >
-      <CardHeader className="p-2 ">
+        className={clsx(
+          "w-64 h-[100px] border rounded-sm shadow-lg p-0 bg-cover",
+          isActive ? "bg-card-active border-white opacity-100 " : "bg-card-disactive border-gray-400 opacity-90",
+          className
+        )}
+        style={cardBgClass}
+      >
+
+      <CardHeader className="p-2">
         <CardTitle className="flex justify-between text-heading-lg-bold text-white ">
           <span className="text-xl font-bold">{name}</span>
         </CardTitle>
       </CardHeader>
-      <CardContent className="p-2 pt-0">
+      <CardContent className="p-1 pt-0">
         <Progress
-          className="h-3 bg-gray-800 rounded-sm border border-black"
+          className="h-4 bg-gray-800 rounded-sm border border-black"
           value={percent}
           indicatorClassName={`${getIndicatorClass()} rounded-none transition-all`}
         />
