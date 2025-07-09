671a7e66ac683b8bcd68f48a9bbea53d340ce9f1
yuval-itzhak-hls <yuvali@moveohls.com>
2025-07-09 15:42:18 +0300

    disassemble ChoosePokemonBattle component to smaller components.

---
+++ src/pages/home-page/choose-pokemon-panel/BattlePanelFooter.tsx
@@ -0,0 +1,23 @@
+import { GenericButton } from "@/design-system/generic-componenets/button/GenericButton"; 
+
+interface BattlePanelFooterProps {
+  isDisabled: boolean;
+  onStart: () => void;
+}
+
+export const BattlePanelFooter = ({
+  isDisabled,
+  onStart,
+} : BattlePanelFooterProps) => {
+  return (
+    <div className="text-center">
+      <GenericButton
+        type="primary"
+        size="small"
+        text="Start battle"
+        disabled={isDisabled}
+        onClick={onStart}
+      />
+    </div>
+  );
+};
--- src/pages/home-page/choose-pokemon-panel/ChoosePokemonBattlePanel.tsx
+++ src/pages/home-page/choose-pokemon-panel/ChoosePokemonBattlePanel.tsx
@@ -1,9 +1,10 @@
-import React, { useState } from "react";
+import  { useState } from "react";
 import { Dialog, DialogOverlay, DialogContent, DialogTitle } from "@/components/ui/dialog";
 import { useNavigate } from "react-router-dom";
 import { useBattle } from "@/context/BattleContext";
-import type { Pokemon } from "@/hooks/usePokemonsData";
-import { GenericButton } from "@/design-system/generic-componenets/button/GenericButton";
+import type { Pokemon } from "@/hooks/usePokemonsData"; 
+import { BattlePokemonSelection } from "./BattlePokemonSelection";
+import { BattlePanelFooter } from "./BattlePanelFooter";
 
 interface ChoosePokemonBattlePanelProps {
   myPokemons: Pokemon[];
@@ -12,12 +13,12 @@ interface ChoosePokemonBattlePanelProps {
   onClose: () => void;
 }
 
-export const ChoosePokemonBattlePanel: React.FC<ChoosePokemonBattlePanelProps> = ({
+export const ChoosePokemonBattlePanel = ({
   myPokemons,
   allPokemons,
   isOpen,
   onClose,
-}) => {
+}: ChoosePokemonBattlePanelProps) => {
   const [selectedId, setSelectedId] = useState<string | null>(null);
   const navigate = useNavigate();
   const { setBattle } = useBattle();
@@ -25,11 +26,14 @@ export const ChoosePokemonBattlePanel: React.FC<ChoosePokemonBattlePanelProps> =
   const handleSelect = (p: Pokemon) => setSelectedId(p.id);
 
   const handleStart = () => {
-    const user = myPokemons.find(p => p.id === selectedId)!;
-    const pool = allPokemons.filter(
+    if (!selectedId) return; 
+
+    const user = myPokemons.find(p => p.id === selectedId)!; 
+    const optionalOpponents = allPokemons.filter(
       p => p.id !== selectedId && !myPokemons.some(mp => mp.id === p.id)
     );
-    const opponent = pool[Math.floor(Math.random() * pool.length)];
+    
+    const opponent = optionalOpponents[Math.floor(Math.random() * optionalOpponents.length)];
 
     setBattle(user, opponent);
     onClose();
@@ -38,43 +42,23 @@ export const ChoosePokemonBattlePanel: React.FC<ChoosePokemonBattlePanelProps> =
 
   return (
     <Dialog open={isOpen} onOpenChange={open => !open && onClose()}>
-      <DialogOverlay className="fixed inset-0 bg-opacity-50 " />
-      <DialogContent className="fixed top-1/2 left-1/2 w-[500px] max-w-[600px] h-[500px] max-h-[500px] overflow-y-auto bg-white pb-0">
+      <DialogOverlay className="fixed inset-0 bg-opacity-50" />
+      <DialogContent className="fixed top-1/2 left-1/2 w-[500px] max-w-[600px] h-[500px] max-h-[500px] overflow-y-auto bg-white pb-3">
 
         <div className="flex justify-between items-center mb-1">
           <DialogTitle className="text-heading-lg-regular">Choose the Pokemon to battle with</DialogTitle>
         </div>
 
-        <div className="grid grid-cols-3 border-b border-gray-200 pb-2 gap-8 justify-items-center mb-2">
-          {myPokemons.map(p => (
-            <div
-              key={p.id}
-              onClick={() => handleSelect(p)}
-              className="rounded-full cursor-pointer transition"
-            >
-              <img
-                src={p.image}
-                alt={p.name}
-                className={
-                  `rounded-full w-[100px] h-[100px] object-cover ring-2 transition ` +
-                  (selectedId === p.id
-                    ? "ring-blue-500"
-                    : "ring-transparent hover:ring-gray-300")
-                  }
-              />
-            </div>
-          ))}
-        </div>
+        <BattlePokemonSelection
+          pokemons={myPokemons}
+          selectedId={selectedId}
+          onSelect={handleSelect}
+        />
 
-        <div className="text-center">
-          <GenericButton
-            type="primary"
-            size="small"
-            text="Start battle"
-            disabled={!selectedId}
-            onClick={handleStart}
-          />
-        </div>
+        <BattlePanelFooter
+          isDisabled={!selectedId}
+          onStart={handleStart}
+        />
       </DialogContent>
     </Dialog>
   );
